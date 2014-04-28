f1580c940321aea716c59099f65cebb20469e606
Drew Foehn <drew@pixelburn.net>
2014-04-28 11:23:24 -0400

    added gulp and package

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+var paths = {
+  sass: ['./css/**/*.scss']
+};
+
+gulp.task('sass', function(done) {
+  gulp.src('./css/phasher.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./css/'))
+    .on('end', done);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('default', ['sass']);
